056796f8a5c6ad31d7a6c8972faf4ca9426fac25
Afiq Yusof <mafiqyus@vt.edu>
2018-11-29 09:37:12 -0500

    Public lists should now properly come up.
    Also made major changes to user affiliated classes. Mainly with account management. Log in and create account pages should now come up

--- src/java/edu/vt/FacadeBeans/UserFacade.java
+++ src/java/edu/vt/FacadeBeans/UserFacade.java
@@ -1,6 +1,6 @@
 /*
- * Created by Muhammad Afiq Yusof on 2018.11.28  * 
- * Copyright © 2018 Muhammad Afiq Yusof. All rights reserved. * 
+ * Created by Afiq Yusof on 2018.11.29  * 
+ * Copyright © 2018 Afiq Yusof. All rights reserved. * 
  */
 package edu.vt.FacadeBeans;
 
@@ -26,6 +26,45 @@ public class UserFacade extends AbstractFacade<User> {
 
     public UserFacade() {
         super(User.class);
+    }
+    
+    /**
+     * @param id is the Primary Key of the User entity in a table row in the database.
+     * @return object reference of the User object whose primary key is id
+     */
+    public User getUser(int id) {
+        
+        // The find method is inherited from the parent AbstractFacade class
+        return em.find(User.class, id);
+    }
+
+    /**
+     * @param username is the username attribute (column) value of the user
+     * @return object reference of the User entity whose user name is username
+     */
+    public User findByUsername(String username) {
+        if (em.createQuery("SELECT c FROM User c WHERE c.username = :username")
+                .setParameter("username", username)
+                .getResultList().isEmpty()) {
+            return null;
+        } else {
+            return (User) (em.createQuery("SELECT c FROM User c WHERE c.username = :username")
+                    .setParameter("username", username)
+                    .getSingleResult());
+        }
+    }
+
+    /**
+     * Deletes the User entity whose primary key is id
+     * @param id is the Primary Key of the User entity in a table row in the database.
+     */
+    public void deleteUser(int id) {
+        
+        // The find method is inherited from the parent AbstractFacade class
+        User user = em.find(User.class, id);
+        
+        // The remove method is inherited from the parent AbstractFacade class
+        em.remove(user); 
     }
     
 }
--- src/java/edu/vt/FacadeBeans/UserPhotoFacade.java
+++ src/java/edu/vt/FacadeBeans/UserPhotoFacade.java
@@ -1,10 +1,11 @@
 /*
- * Created by Muhammad Afiq Yusof on 2018.11.28  * 
- * Copyright © 2018 Muhammad Afiq Yusof. All rights reserved. * 
+ * Created by Afiq Yusof on 2018.11.29  * 
+ * Copyright © 2018 Afiq Yusof. All rights reserved. * 
  */
 package edu.vt.FacadeBeans;
 
 import edu.vt.EntityBeans.UserPhoto;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,13 +20,44 @@ public class UserPhotoFacade extends AbstractFacade<UserPhoto> {
     @PersistenceContext(unitName = "CloudListPU")
     private EntityManager em;
 
+    // @Override annotation indicates that the super class AbstractFacade's getEntityManager() method is overridden.
     @Override
     protected EntityManager getEntityManager() {
         return em;
     }
 
+    /* 
+    This constructor method invokes the parent abstract class AbstractFacade.java's
+    constructor method AbstractFacade, which in turn initializes its entityClass instance
+    variable with the UserPhoto class object reference returned by the UserPhoto.class.
+     */
     public UserPhotoFacade() {
         super(UserPhoto.class);
     }
+    
+    /*
+    *********************************************************
+    The following method is added to the auto generated code.
+    *********************************************************
+     */
+    /**
+     * @param primaryKey is the Primary Key of the User entity in a table row in the database.
+     * @return a list of photos associated with the User whose primary key is primaryKey
+     */
+    public List<UserPhoto> findPhotosByUserPrimaryKey(Integer primaryKey) {
+
+        return (List<UserPhoto>) em.createNamedQuery("UserPhoto.findPhotosByUserDatabasePrimaryKey")
+                .setParameter("primaryKey", primaryKey)
+                .getResultList();
+    }
+
+    /* The following methods are inherited from the parent AbstractFacade class:
+    
+        create
+        edit
+        find
+        findAll
+        remove
+     */
     
 }
